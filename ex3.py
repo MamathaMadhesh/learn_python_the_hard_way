1d275da9879cc4804e346a3c71c119d78e1f07c7
Mamatha <mamatha.madhesh@gmail.com>
2019-12-24 18:08:52 +0530

    comments

---
+++ ex3.py
@@ -0,0 +1,11 @@
+print("I will now count my balls:")
+print("Balls:",25+30/6)
+
+print("Now I will count the Bats:")
+print("Bats:",50+40)
+
+print("Now I will count the jersy's:")
+print("jersy's:",3+3+4+2-6*2+1/2)
+
+print("Is it true that 3+2<5-7?")
+print(3+2<5-7)
