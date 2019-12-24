20bf09ad8b4a69125dd7299638856c7e2bdc3177
Mamatha <mamatha.madhesh@gmail.com>
2019-12-24 18:06:33 +0530

    first program

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+print("Hello Again")
+print("I Like typing this.")
+print("This is fun")
+print("Yes! it is printing")
+print("I'd much rather you 'not'.")
+print('"I said" do not touch this.')
