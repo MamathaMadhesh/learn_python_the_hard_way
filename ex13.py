bd7377ee6ab406fd6708e569ea89afbaa2d5f3e0
Mamatha <mamatha.madhesh@gmail.com>
2019-12-27 16:10:52 +0530

    Parameters,Unpacking and Variables

---
+++ ex13.py
@@ -0,0 +1,10 @@
+# Parameters,Unpacking and Variables
+
+from sys import argv
+
+script,first,second,third=argv
+
+print("The script is",script)
+print("The first arg",first)
+print("The second arg",second)
+print("The Third arg",third)
