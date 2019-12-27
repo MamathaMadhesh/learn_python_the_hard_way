477772f47b0c586f2a29c28b458f7aaf6d99da03
Mamatha <mamatha.madhesh@gmail.com>
2019-12-27 16:10:01 +0530

    prompting to people

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# prompting to people
+
+age=int(input("How old are you?"))
+height=input("How tall are you?")
+weight=input("How much do you weight?")
+
+print(f"So you are {age} Old,{height} height and {weight} heavy")
