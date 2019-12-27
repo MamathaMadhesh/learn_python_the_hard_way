8841baa39c4ddef88fd6aa59c27f6e09f18d6836
Mamatha <mamatha.madhesh@gmail.com>
2019-12-27 15:41:35 +0530

    More Variables and Printing

---
+++ ex5.py
@@ -0,0 +1,16 @@
+# More Variables and Printing
+my_name='Mamatha'
+my_age=22
+my_weight=38
+my_height=5.2
+my_hair='silky'
+my_eyes="black"
+my_teeth='white'
+
+print(f"Lets talk about {my_name}.Age is {my_age}")
+print(f"My height is{my_height} and weight is{my_weight}.")
+print(f"My eyea are {my_eyes} and hair is {my_hair}")
+print(f"And my teeth is {my_teeth}")
+
+total=my_age+my_height+my_weight
+print(f"If I Add {my_age},{my_height} and {my_weight} I get {total}.")
