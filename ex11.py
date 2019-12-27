dc0bb08961846f574868fa48df9d4c897410e03e
Mamatha <mamatha.madhesh@gmail.com>
2019-12-27 16:08:58 +0530

    Asking questions using input () method

---
+++ ex11.py
@@ -0,0 +1,9 @@
+# Asking questions using input () method
+print("How old are you?")
+age=input()
+print("How tall are you?",end='')
+height=input()
+print("How much do yo weight?",end='')
+weight=input()
+
+print(f"So,you\'re {age} old,{height} height and {weight} weight")
