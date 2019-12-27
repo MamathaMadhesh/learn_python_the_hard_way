52ab4ae848053fff3a9fb7e5fac6b26d5ccc7b9b
Mamatha <mamatha.madhesh@gmail.com>
2019-12-27 16:06:46 +0530

    Escape sequences

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Escape sequences
+
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nsep\nOct\nNov\nDec"
+print(days)
+print(months)
+
+print("Here the days {}.".format(days))
+print(f"Here the Months {months}")
+
+print("""
+Here somethig is going on with
+triple quotes. we are able to type the line
+more than 4 or 5 and even 6 lines.""")
